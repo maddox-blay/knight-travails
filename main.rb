ddd923f936a5ea65a2f8ffff516f74659e116831
maddox-blay <156577870+maddox-blay@users.noreply.github.com>
2025-06-23 20:04:34 +0000

    added edge_generator and breadth_first functions

--- main.rb
+++ main.rb
@@ -1,1 +1,39 @@
+def edge_generator((a,b))
+  edges = [
+    [a - 2, b - 1],
+    [a - 1, b - 2],
+    [a + 1, b - 2],
+    [a + 2, b - 1],
+    [a + 2, b + 1],
+    [a + 1, b + 2],
+    [a - 1, b + 2],
+    [a - 2, b + 1]
+  ]
 
+  valid_edges = edges.select {|edge| edge.all? {|e| (0..7).include?e}}
+
+  return valid_edges
+end
+
+def breadth_first(start, finish)
+  visited_nodes = []
+  queue = []
+
+  visited_nodes << start
+  queue << start
+
+  until queue.empty?
+    current = queue.shift
+    puts "[#{current.join(",")}]"
+    edge_generator(current).each do |neighbour|
+      unless visited_nodes.include? neighbour
+        visited_nodes << neighbour
+        queue << neighbour
+      end
+    end
+  end
+end
+
+breadth_first([3,4], [4,2])
+puts [" "," "]
+puts edge_generator [1,3]
